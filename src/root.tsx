9406e7324753bf97b03fae8df71de0604f460050
Davide Diaconu <davidediak@gmail.com>
2023-11-04 16:15:13 +0200

    installed flowbite

--- src/root.tsx
+++ src/root.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useVisibleTask$ } from "@builder.io/qwik";
 import {
   QwikCityProvider,
   RouterOutlet,
@@ -7,14 +7,14 @@ import {
 import { RouterHead } from "./components/router-head/router-head";
 
 import "./global.css";
+import { initFlowbite } from 'flowbite';
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Don't remove the `<head>` and `<body>` elements.
-   */
+
+  // initialise the event listeners for the data attributes on render
+  useVisibleTask$(() => {
+    initFlowbite();
+  });
 
   return (
     <QwikCityProvider>
@@ -22,10 +22,10 @@ export default component$(() => {
         <meta charSet="utf-8" />
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
+        <ServiceWorkerRegister />
       </head>
       <body lang="en">
         <RouterOutlet />
-        <ServiceWorkerRegister />
       </body>
     </QwikCityProvider>
   );
